fc5de6620ccc8e41bda7e7853447ce788c591bf1
Wail Solaiman <wailsolaiman@hotmail.com>
2024-09-05 08:56:02 +0200

    add link to each template/tool

--- app/dashboard/_components/TemplateCard.tsx
+++ app/dashboard/_components/TemplateCard.tsx
@@ -1,18 +1,21 @@
 import React from 'react'
 import { TEMPLATE } from './TemplateListSection'
 import Image from 'next/image'
+import Link from 'next/link'
 
 const TemplateCard = (item: TEMPLATE) => {
 	return (
-		<div
-			className='p-5 shadow-md rounded-md border bg-white 
+		<Link href={`/dashboard/content/${item?.slug}`}>
+			<div
+				className='p-5 shadow-md rounded-md border bg-white 
         flex flex-col justify-center items-center gap-3 cursor-pointer hover:scale-105 transition-all'>
-			<Image src={item.icon} alt={item.name} width={50} height={50} />
-			<h2 className='font-medium text-lg'>{item.name}</h2>
-			<p className='text-gray-500 line-clamp-3 text-center'>
-				{item.desc}
-			</p>
-		</div>
+				<Image src={item.icon} alt={item.name} width={50} height={50} />
+				<h2 className='font-medium text-lg'>{item.name}</h2>
+				<p className='text-gray-500 line-clamp-3 text-center'>
+					{item.desc}
+				</p>
+			</div>
+		</Link>
 	)
 }
 

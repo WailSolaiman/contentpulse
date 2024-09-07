9d7cb0f6f49f70a6e6cc330fb0a9a84207db5b0c
Wail Solaiman <wailsolaiman@hotmail.com>
2024-09-07 19:56:59 +0200

    update outputsection component with a text-editor to display the generating results and edit them

--- app/dashboard/content/_components/OutputSection.tsx
+++ app/dashboard/content/_components/OutputSection.tsx
@@ -1,7 +1,35 @@
-import React from 'react'
+'use client'
+
+import React, { useRef } from 'react'
+import { Editor } from '@toast-ui/react-editor'
+
+import '@toast-ui/editor/dist/toastui-editor.css'
+import { Button } from '@/components/ui/button'
+import { Copy } from 'lucide-react'
 
 const OutputSection = () => {
-	return <div>OutputSection</div>
+	const editorRef = useRef<Editor>(null)
+
+	return (
+		<div className='bg-white shadow-lg border rounded-lg'>
+			<div className='flex justify-between items-center p-5'>
+				<h2 className='font-medium'>Your Result</h2>
+				<Button className='flex gap-2'>
+					<Copy className='w-4 h-4' /> Copy{' '}
+				</Button>
+			</div>
+			<Editor
+				ref={editorRef}
+				initialValue='Your result will appear here.'
+				height='600px'
+				initialEditType='wysiwyg'
+				useCommandShortcut={true}
+				onChange={() =>
+					console.log(editorRef.current?.getInstance().getMarkdown())
+				}
+			/>
+		</div>
+	)
 }
 
 export default OutputSection
--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	images: {
-		domains: ['cdn-icons-png.flaticon.com'],
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'cdn-icons-png.flaticon.com',
+				pathname: '**',
+			},
+		],
 	},
 }
 
